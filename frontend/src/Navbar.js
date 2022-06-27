3efbaeec9aba552a08dcc6a7b90a6016e467dff5
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2022-06-27 22:55:09 +0530

    finish navbar completely

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,8 +1,9 @@
 import './build.css';
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from 'react';
 
 const NavListItem = (props) => {
-  return <div className="p-4 bg-blue-500/60 hover:bg-blue-600/60 hover:cursor-pointer rounded-full border-blue-700/50 border-4">{props.children}</div>;
+  return <div className="text-center p-4 bg-blue-500/60 hover:bg-blue-600/60 hover:cursor-pointer rounded-full border-blue-700/50 border-4">{props.children}</div>;
 }
 
 const NavItem = (props) => {
@@ -22,9 +23,29 @@ const NavItem = (props) => {
   </div>;
 }
 
+const MobileNavItem = (props) => {
+  const handleClick = () => {
+    if (props.index === props.open) {
+      props.setOpen(null);
+      return;
+    }
+    props.setOpen(props.index);
+  }
+  return <div>
+    <div className="h-20 m-auto bg-blue-300/50 rounded-full hover:bg-blue-400 hover:cursor-pointer flex border-blue-700/50 border-4" onClick={handleClick}>
+      <span className="m-auto">{props.children}</span>
+    </div>
+    {(props.listItems && props.open === props.index) ? <div className="transition-all flex flex-col space-y-0.5">
+      {props.listItems.length ? props.listItems.map((child, i) => {
+        return <NavListItem key={i}>{child.props.children}</NavListItem>;
+      }) : <NavListItem>{props.listItems.props.children}</NavListItem>}
+    </div> : <div></div>}
+  </div>;
+}
+
 const NavbarDesktop = (props) => {
   return (<div className="flex space-x-4 shadow-2xl text-center bg-blue-300/70 rounded-full box-content p-4 h-20">
-      {!props.logo ? <div className="flex min-w-1/6 bg-blue-400/50 rounded-full hover:bg-blue-400 hover:cursor-pointer border-blue-700/50 border-4">
+      {props.logo ? <div className="flex min-w-1/8 bg-blue-400/50 rounded-full hover:bg-blue-400 hover:cursor-pointer border-blue-700/50 border-4">
         <img src={props.thumbnail} className="m-auto h-4/5" alt="" />
       </div> : <div></div>}
       {props.children.map((child, i) => {
@@ -34,29 +55,32 @@ const NavbarDesktop = (props) => {
 }
 
 const NavbarMobile = (props) => {
+  const [open, setOpen] = useState(false);
+  const [whichOpen, setWhichOpen] = useState(null);
+  const handleClick = () => {
+    setOpen(!open);
+  }
   return (<div>
-    <input type="checkbox" className="hidden peer" id="menu" />
-    <label htmlFor="menu" className="group">
-      <div className="flex space-x-4 shadow-2xl bg-blue-300/70 hover:bg-blue-300/100 hover:cursor-pointer rounded-full box-content p-4 h-20">
-        <AiOutlineMenu className="m-auto text-black hover:text-gray-600" size={50}/>
-        <div className="hidden group-checked:block">
-          hello
-        </div>
-      </div>
-    </label>
-  </div>);
+    <div onClick={handleClick} className="flex space-x-4 shadow-2xl bg-blue-300/70 rounded-full box-content p-4 h-20">
+      {open ? 
+      <AiOutlineClose className="m-auto text-black hover:text-gray-600 hover:cursor-pointer relative" size={50} /> :
+      <AiOutlineMenu  className="m-auto text-black hover:text-gray-600 hover:cursor-pointer relative" size={50} /> } 
+    </div>
+    {open ? <div className="space-y-0.5">
+      {props.children.map((child, i) => {
+        return <MobileNavItem key={i} index={i} open={whichOpen} setOpen={setWhichOpen} listItems={child.props.children[1].props.children}>{child.props.children[0].props.children}</MobileNavItem>;
+      })}
+    </div> : <div></div>}
+    </div>);
 }
 
 const Navbar = (props) => {
   return <div className="nav">
-    <div className="hidden md:block">
-      <NavbarDesktop {...props}/>
+    <div className="hidden sm:block">
+      <NavbarDesktop logo={true} {...props}/>
     </div>
     <div className="block sm:hidden">
-      <NavbarMobile />
-    </div>
-    <div className="hidden sm:block md:hidden">
-      <NavbarDesktop logo={true} {...props} />
+      <NavbarMobile {...props}/>
     </div>
   </div>;
 }
